9c95dc5fd461763bfc09919c0db02ac3d43e9e92
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-10-14 12:21:34 +0530

    "Commit"
    Solution to set zeroes

---
+++ Competitive Programming/Q2_ZeroMatrix/set_matrix.java
@@ -0,0 +1,23 @@
+public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1||col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        
+    }
