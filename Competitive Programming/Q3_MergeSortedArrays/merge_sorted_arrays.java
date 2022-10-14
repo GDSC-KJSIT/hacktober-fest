ec3852db536c54a7bb8eb6e00907a05c0ec9783a
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-10-14 20:38:30 +0530

    Merge Sorted Arrays Solution

---
+++ Competitive Programming/Q3_MergeSortedArrays/merge_sorted_arrays.java
@@ -0,0 +1,42 @@
+class Solution {
+public static void main(String[] args){
+	int[] nums1 = {1,2,3,0,0,0};
+	int m = 3;
+	int[] nums2 = {2,5,6};
+	int n = 3;
+
+}
+	
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int k=0;
+        int[] merged = new  int[m+n];
+        while (i < m && j < n){
+            if (nums1[i] <= nums2[j]){
+                merged[k] = nums1[i];
+                i++;
+            } else {
+                merged[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i<m){
+            merged[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        while (j<n){
+            merged[k] = nums2[j];
+            j++;
+            k++;
+        }
+        
+        for (int ele = 0;ele<nums1.length;ele++){
+            nums1[ele] = merged[ele];
+        }
+    }
+}
