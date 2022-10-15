40c57f3feb4f90eae080b1598d30d4867f5b169f
Dhyey Shah <83686569+DhyeyShah794@users.noreply.github.com>
2022-10-16 02:51:08 +0530

    Solved Q1, Q2 & Q3 in CP

---
+++ Competitive Programming/Q1_Factorial/d-factorial.py
@@ -0,0 +1,12 @@
+def factorial(num):
+    """Takes a number and returns the factorial of that number using recursion."""
+    if type(num) != int or num < 0:  # Check if the input is an integer and is positive
+        return "Not defined"
+    if num <= 1:  # Base case
+        return 1
+    else:  # Recursive case
+        return num * factorial(num-1)
+
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
