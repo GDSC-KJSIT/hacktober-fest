c794ae193faddb0d9401d102572df312699e7c9f
Kartik Raina <96180683+KARTIK-RAINA5802@users.noreply.github.com>
2022-10-16 11:38:15 +0530

    Added Solution to Zero Matrix in c++

---
+++ Competitive Programming/Q2_ZeroMatrix/zeroMatrix.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int rows = matrix.size(), cols = matrix[0].size();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+
+                int ind = i - 1;
+                while (ind >= 0)
+                {
+                    if (matrix[ind][j] != 0)
+                    {
+                        matrix[ind][j] = -1;
+                    }
+                    ind--;
+                }
+                ind = i + 1;
+                while (ind < rows)
+                {
+                    if (matrix[ind][j] != 0)
+                    {
+                        matrix[ind][j] = -1;
+                    }
+                    ind++;
+                }
+                ind = j - 1;
+                while (ind >= 0)
+                {
+                    if (matrix[i][ind] != 0)
+                    {
+                        matrix[i][ind] = -1;
+                    }
+                    ind--;
+                }
+                ind = j + 1;
+                while (ind < cols)
+                {
+                    if (matrix[i][ind] != 0)
+                    {
+                        matrix[i][ind] = -1;
+                    }
+                    ind++;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (matrix[i][j] <= 0)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<vector<int>> arr;
+    int n, m, temp = 0;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> v;
+        for (int j = 0; j < m; j++)
+        {
+            cin >> temp;
+            v.push_back(temp);
+        }
+        arr.push_back(v);
+    }
+    setZeroes(arr);
+    cout << "The Final Matrix is " << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
