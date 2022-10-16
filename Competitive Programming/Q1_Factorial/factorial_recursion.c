ee147a58bf8a5c52d3fdbcce4f82676eb096afcd
krishnagithub03 <agrawalkrishna030@gmail.com>
2022-10-16 16:12:51 +0530

    Factorial Of Number Using C

---
+++ Competitive Programming/Q1_Factorial/factorial_recursion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int fact(int x);
+int main(){
+    int a=5;
+    printf("the factorial of %d is %d",a,fact(a));
+    return 0;
+}
+
+int fact(int x){
+    if(x==1 || x==0){ //base condition
+        return 1;
+    }
+    else{
+        return x*fact(x-1); // function calling itself 
+    }
+}
