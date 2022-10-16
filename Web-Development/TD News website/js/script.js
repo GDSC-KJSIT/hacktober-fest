181a68d33903896b8f3634616ea0d9f65e4cd254
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2022-10-16 12:11:04 +0530

    added solution to web dev q1

---
+++ Web-Development/TD News website/js/script.js
@@ -0,0 +1,213 @@
+const BBTN = document.getElementById("business");
+const SBTN = document.getElementById("sports");
+const entertainmentBtn = document.getElementById("Entertainment");
+const technologyBtn = document.getElementById("Technology");
+const searchBtn = document.getElementById("search-btn");
+
+const newsQuery = document.getElementById("newsQuery");
+const newsType = document.getElementById("newsType");
+const newsdetails = document.getElementById("newsdetails");
+
+var newsDataArr = [];
+
+const API_KEY = "240983b98f9049beba40c7072604770f";
+const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
+const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
+const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
+const SPORTS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=";
+const ENTERTAINMENT_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=";
+const TECHNOLOGY_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=8&apiKey=";
+const SEARCH_NEWS = "https://newsapi.org/v2/everything?q=";
+
+window.onload = function() {
+    newsType.innerHTML="<h4>Headlines</h4>";
+    fetchHeadlines();
+};
+
+
+BBTN.addEventListener("click",function(){
+    newsType.innerHTML="<h4>Business</h4>";
+    fetchBusinessNews();
+});
+
+SBTN.addEventListener("click",function(){
+    newsType.innerHTML="<h4>Sports</h4>";
+    fetchSportsNews();
+});
+
+entertainmentBtn.addEventListener("click",function(){
+    newsType.innerHTML="<h4>Entertainment</h4>";
+    fetchEntertainmentNews();
+});
+
+technologyBtn.addEventListener("click",function(){
+    newsType.innerHTML="<h4>Technology</h4>";
+    fetchTechnologyNews();
+});
+
+searchBtn.addEventListener("click",function(){
+    newsType.innerHTML="<h4>Search : "+newsQuery.value+"</h4>";
+    fetchQueryNews();
+});
+
+const fetchHeadlines = async () => {
+    const response = await fetch(HEADLINES_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+
+
+const fetchBusinessNews = async () => {
+    const response = await fetch(BUSINESS_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+const fetchEntertainmentNews = async () => {
+    const response = await fetch(ENTERTAINMENT_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        console.log(myJson);
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+const fetchSportsNews = async () => {
+    const response = await fetch(SPORTS_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+const fetchTechnologyNews = async () => {
+    const response = await fetch(TECHNOLOGY_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+const fetchQueryNews = async () => {
+
+    if(newsQuery.value == null)
+        return;
+
+    const response = await fetch(SEARCH_NEWS+encodeURIComponent(newsQuery.value)+"&apiKey="+API_KEY);
+    newsDataArr = [];
+    if(response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        //error handle
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+function displayNews() {
+
+    newsdetails.innerHTML = "";
+
+    // if(newsDataArr.length == 0) {
+    //     newsdetails.innerHTML = "<h5>No data found.</h5>"
+    //     return;
+    // }
+
+    newsDataArr.forEach(news => {
+
+        var date = news.publishedAt.split("T");
+        
+        var col = document.createElement('div');
+        col.className="col-sm-12 col-md-4 col-lg-3 p-2 card";
+
+        var card = document.createElement('div');
+        card.className = "p-2";
+
+        var image = document.createElement('img');
+        image.setAttribute("height","matchparent");
+        image.setAttribute("width","100%");
+        image.src=news.urlToImage;
+
+        var cardBody = document.createElement('div');
+        
+        var newsHeading = document.createElement('h5');
+        newsHeading.className = "card-title";
+        newsHeading.innerHTML = news.title;
+
+        var dateHeading = document.createElement('h6');
+        dateHeading.className = "text-primary";
+        dateHeading.innerHTML = date[0];
+
+        var discription = document.createElement('p');
+        discription.className="text-muted";
+        discription.innerHTML = news.description;
+
+        var link = document.createElement('a');
+        link.className="btn btn-dark";
+        link.setAttribute("target", "_blank");
+        link.href = news.url;
+        link.innerHTML="Read more";
+
+        cardBody.appendChild(newsHeading);
+        cardBody.appendChild(dateHeading);
+        cardBody.appendChild(discription);
+        cardBody.appendChild(link);
+
+        card.appendChild(image);
+        card.appendChild(cardBody);
+
+        col.appendChild(card);
+
+        newsdetails.appendChild(col);
+    });
+
+}
--- Web-Development/web-d-contributor.md
+++ Web-Development/web-d-contributor.md
@@ -1,1 +1,7 @@
 ### Enter your name college branch and in short what pull request you made for this folder
+
+1. * Name - Trushil Dhokiya
+   * College - K J Somaiya College of Engineering and Information Technology, Sion
+   * Branch - COMPS
+   * Domain - Web Development
+   * Contribution - made interactive news Website using NewsAPI
