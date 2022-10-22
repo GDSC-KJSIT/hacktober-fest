4c0f39994bf78996334be892da65b0d141bfa0a1
SnappyDragon18 <pg61151@gmail.com>
2022-10-22 22:55:26 +0530

    Changed C style array declaration

--- Competitive Programming/Q3_MergeSortedArrays/MergeSortedArraysSD.java
+++ Competitive Programming/Q3_MergeSortedArrays/MergeSortedArraysSD.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeSortedArraysSD {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter size of first sorted array:");
